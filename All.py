9cb2c2232cf0200a2e6065c2fbfc0e2cec407c7b
mjhernandez.24 <mjhernandez.24@est.ucab.edu.ve>
2025-07-07 21:09:27 -0400

    AAAAAAHHHHHHHH

--- All.py
+++ All.py
@@ -177,6 +177,51 @@ def merge_sort(array, columna=0):
             k += 1
 
 
+def vender_producto(inventario):
+    print("Busca el producto que deseas vender:")
+    #productos = buscar_inventario(inventario)
+    metodo = input("Busca por (1) Código o (2) Nombre: ")
+    
+    while metodo != "1" and metodo != "2":
+        print("Error: Introduce una opcion valida. ")
+        metodo = input("Busca por (1) Código o (2) Nombre: ")   
+    producto_buscar = input("Ingrese el producto a buscar: ")
+
+    productos_encontrados = []
+    
+    for producto in inventario:
+        if metodo == "1" and str(producto[0]) == producto_buscar:
+            productos_encontrados.append(producto)
+        elif metodo == "2" and producto_buscar in producto[1].lower():
+            productos_encontrados.append(producto)
+            
+    if productos_encontrados:
+        for x in productos_encontrados:
+            print(x)
+    productos= productos_encontrados
+    producto_encontrado = productos[0]
+    nombre = producto_encontrado[1]
+    cantidad_disponible = producto_encontrado[3]
+
+    print(f"\nProducto seleccionado: {nombre}")
+    print(f"Cantidad disponible: {cantidad_disponible}")
+
+    cantidad_vender = input("Ingrese la cantidad a vender: ")
+    cantidad_vender = int(cantidad_vender)
+
+    while cantidad_vender > cantidad_disponible:
+        print(" No puedes vender más de lo que tienes en inventario.")
+        cantidad_vender = input("Ingrese nuevamente la cantidad a vender: ")
+        cantidad_vender = int(cantidad_vender)
+    while (cantidad_vender <= 0):
+        print("Error no puedes vender cantidades negativas.")
+        cantidad_vender = input("Ingrese nuevamente la cantidad a vender: ")
+        cantidad_vender = int(cantidad_vender)
+    else:
+        producto_encontrado[3] -= cantidad_vender
+        print(f"Venta realizada. Quedan {producto_encontrado[3]} unidades de {nombre}.")
+
+
 def interfaz():
     print("\n--- SISTEMA DE GESTION DE INVENTARIO ---\n")
     print("1. Ver Todos los productos del inventario")
@@ -202,7 +247,7 @@ def main():
         elif opciones =="3":
             ingresarProducto()
         elif opciones == "4":
-            pass
+            vender_producto(inventarioFormateado)
         elif opciones=="5":
             pass
         elif opciones =="6": 
